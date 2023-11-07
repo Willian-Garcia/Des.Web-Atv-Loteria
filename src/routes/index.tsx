fdbf4b1a80be8fd5035a12319c043d05087fde67
Willian-Garcia <williangarcia710@hotmail.com>
2023-11-06 21:34:20 -0300

    Inserção do Menu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,12 @@ import Megasena from "../pages/Megasena";
 import Lotofacil from "../pages/Lotofacil";
 import Quina from "../pages/Quina";
 import "./index.css";
+import Menu from "../components/Menu";
 
 export default function Rotas(){
     return(
         <BrowserRouter>
+            <Menu/>
             <Routes>
                 <Route path = "/megasena" element={<Megasena/>}/>
                 <Route path = "/lotofacil" element={<Lotofacil/>}/>
